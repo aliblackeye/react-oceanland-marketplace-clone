8225a4ec09eb03efd4d07ba22f284f67d233d187
baython <ali.blackeye@hotmail.com>
2022-09-18 19:40:03 +0300

    Hamburger menu added.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import HomeScreen from "./screens/HomeScreen/HomeScreen.jsx";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 import "./app.scss";
 
-import './responsive.scss';
+import "./responsive.scss";
 
 function App() {
-
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-      </Routes> 
+      </Routes>
       <Footer />
     </Router>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { IoWalletOutline } from 'react-icons/io5';
-import { GiHamburgerMenu } from 'react-icons/gi';
 
 import { FiPlay } from 'react-icons/fi';
 
@@ -11,7 +10,7 @@ export default function Header() {
 
   // change nav style when scrolling
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
 
   const changeHeader = () => {
     if (window.scrollY >= 10) {
@@ -25,7 +24,11 @@ export default function Header() {
   window.addEventListener('scroll', changeHeader);
 
   const handleClick = () => {
-    
+    setIsMenuOpened(!isMenuOpened);
+  }
+
+  const closeMenu = () => {
+    setIsMenuOpened(false);
   }
 
   return (
@@ -49,11 +52,34 @@ export default function Header() {
         <div className="buttons">
           <button className="btn btn-primary btn-with-icon"><IoWalletOutline size={16} color='#fff' /> Connect Wallet</button>
           <button className="btn btn-secondary btn-with-icon"><FiPlay size={16} color='#fff' /> Play Game</button>
-          <div className="hamburger-menu" onClick={(e) => handleClick(e)}>
-            <GiHamburgerMenu color='#197aff' size={24} />
+          <div className={`hamburger-menu-btn ${isMenuOpened ? "active" : ""}`} onClick={() => handleClick()}>
+            <div className="lines">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
           </div>
         </div>
+
+
       </div>
+
+      <nav className={`hamburger-menu ${isMenuOpened ? "active" : ""}`}>
+        <div className="hamburger-menu-wrapper">
+          <div className="logo">
+            <img src="./images/header/logo.png" alt="" />
+          </div>
+          <Link to='/#' className="nav-link">Marketplace</Link>
+          <Link to='/#' className="nav-link">Swap</Link>
+          <Link to='/#' className="nav-link">Stake</Link>
+          <Link to='/#' className="nav-link">Pool</Link>
+          <Link to='/#' className="nav-link">Farm</Link>
+          <Link to='/#' className="nav-link">Booster</Link>
+          <button className="btn btn-secondary btn-with-icon"><FiPlay size={16} color='#fff' /> Play Game</button>
+          <button className="btn btn-light btn-with-icon"><IoWalletOutline size={16} color='#197AFF' /> Connect Wallet</button>
+        </div>
+      </nav>
     </header>
+
   )
 }
